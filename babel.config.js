184dbea2533e84c152fcae2229dcefa817ea21c4
arya <aryadharmawm22@gmail.com>
2026-01-01 13:26:15 +0800

    init build

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,25 @@ module.exports = function (api) {
   return {
     presets: ["babel-preset-expo"],
     plugins: [
-      "react-native-reanimated/plugin", // Must be last
+      [
+        "module-resolver",
+        {
+          root: ["./src"],
+          extensions: [".ios.js", ".android.js", ".js", ".ts", ".tsx", ".json"],
+        },
+      ],
+      [
+        "module:react-native-dotenv",
+        {
+          moduleName: "@env",
+          path: ".env",
+          blacklist: null,
+          whitelist: null,
+          safe: false,
+          allowUndefined: true,
+        },
+      ],
+      "react-native-reanimated/plugin",
     ],
   };
 };
